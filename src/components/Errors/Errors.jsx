7899925715f4da58b46baf70dea525bd7c61a203
Ivan Kuhta <filmplex22342@gmail.com>
2023-03-22 11:31:18 +0200

    fix: removed command 'return'

--- src/components/Errors/Errors.jsx
+++ src/components/Errors/Errors.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 import styles from './errors.module.css';
 
-const Errors = ({ errors }) => {
-	return (
-		errors && (
-			<ul className={styles.list}>
-				{errors.map((error, index) => (
-					<li key={index}>{error.message}</li>
-				))}
-			</ul>
-		)
+const Errors = ({ errors }) =>
+	errors &&
+	errors.length > 0 && (
+		<ul className={styles.list}>
+			{errors.map((error, index) => (
+				<li key={index}>{error.message}</li>
+			))}
+		</ul>
 	);
-};
 
 export default Errors;
