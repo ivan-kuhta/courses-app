8ec8ef91018be21ac4102cd644c6ffb7010fa584
Ivan Kuhta <filmplex22342@gmail.com>
2023-03-24 09:18:01 +0200

    add: added registration logic

---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Button from '../../common/Button/Button';
+import Input from '../../common/__Input/Input';
+import { URL_API } from '../../constants';
+import Errors from '../Errors/Errors';
+
+import styles from './registration.module.css';
+
+const Registration = () => {
+	const navigate = useNavigate();
+
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [errors, setErrors] = useState([]);
+
+	const fetchNewUser = async () => {
+		const response = await fetch(`${URL_API}/register`, {
+			method: 'POST',
+			body: JSON.stringify({
+				name,
+				email,
+				password,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const { successful, errors } = await response.json();
+		if (successful) {
+			navigate('/login');
+		} else {
+			setErrors(errors.map((message) => ({ message })));
+		}
+	};
+
+	const handlerSubmit = async (e) => {
+		e.preventDefault();
+		fetchNewUser();
+	};
+
+	return (
+		<div className={styles.container}>
+			<form className={styles.form} onSubmit={handlerSubmit}>
+				<h1 className={styles.title}>Registration</h1>
+				<Errors errors={errors} />
+				<Input
+					labelText={'Name'}
+					placeholdetText={'Enter name'}
+					value={name}
+					onChange={({ target: { value } }) => setName(value)}
+				/>
+				<Input
+					name={'email'}
+					labelText={'Email'}
+					placeholdetText={'Enter email'}
+					value={email}
+					onChange={({ target: { value } }) => setEmail(value)}
+				/>
+				<Input
+					type={'password'}
+					labelText={'Password'}
+					placeholdetText={'Enter password'}
+					value={password}
+					onChange={({ target: { value } }) => setPassword(value)}
+				/>
+				<Button type={'submit'} text={'Registration'} className={styles.btn} />
+				<p>
+					If you have an account you can <Link to='/login'>Login</Link>
+				</p>
+			</form>
+		</div>
+	);
+};
+
+export default Registration;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import Courses from './components/Courses/Courses';
+import Registration from './components/Registration/Registration';
 
 const routes = [
 	{
 		path: '/',
 		element: <Courses />,
+	},
+	{
+		path: 'registration',
+		element: <Registration />,
+	},
+	{
+		path: 'login',
+		element: <div>Login</div>,
 	},
 ];
 
