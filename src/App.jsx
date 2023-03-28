61d2a8ffac00220a4bc75d22d24e2e03a2cf4d8d
Ivan Kuhta <filmplex22342@gmail.com>
2023-03-28 17:29:37 +0300

    add: added store in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRoutes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { checkAuth } from './store/user/actionCreators';
+import { fetchCourses } from './store/courses/actionCreators';
+import { fetchAuthors } from './store/authors/actionCreators';
+import { getIsAuth } from './store/user/selectors';
+import { getLoadingUser } from './store/loadings/selectors';
 
 import Header from './components/Header/Header';
 
@@ -8,7 +15,23 @@ import routes from './routes';
 import './App.css';
 
 const App = () => {
+	const dispatch = useDispatch();
 	const routesElement = useRoutes(routes);
+	const isAuth = useSelector(getIsAuth);
+	const isLoadingUser = useSelector(getLoadingUser);
+
+	useEffect(() => {
+		dispatch(checkAuth());
+	}, [dispatch]);
+
+	useEffect(() => {
+		if (isAuth) {
+			dispatch(fetchCourses);
+			dispatch(fetchAuthors);
+		}
+	}, [dispatch, isAuth]);
+
+	if (isLoadingUser) return <p>Loading...</p>;
 
 	return (
 		<div className='app'>
